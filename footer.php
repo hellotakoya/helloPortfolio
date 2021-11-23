77445fbd887959f3ebf1d7ad015625345f22e39f
hellotakoya <ypc62@live.jp>
2021-11-23 16:16:04 +0900

    フッターSNSアイコンのスタイル修正

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <div id="footer" class="footer-icons">
-        <a href="https://www.youtube.com/channel/UC4UKZTlWlDbIfT7cmdDQbfA" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube_social_circle_red.png" loading="lazy" width="100" height="100" alt="" class="image-30"></a>
-        <a href="https://twitter.com/hellotakoya" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Twitter social icons - circle - blue.png" loading="lazy" width="100" height="100" alt="" class="image-30"></a>
-        <a href="https://jp.quora.com/profile/Ishii-Takuya" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/1490889680-quora_82507.png" loading="lazy" width="100" height="100" alt="" class="image-30"></a>
-        <a href="/blog" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/blog-icon.png" loading="lazy" width="100" height="100" alt="" class="image-30"></a>
+        <a href="https://www.youtube.com/channel/UC4UKZTlWlDbIfT7cmdDQbfA" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube_social_circle_red.png" loading="lazy" alt="YouTubeのリンク" class="image-30"></a>
+        <a href="https://twitter.com/hellotakoya" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Twitter social icons - circle - blue.png" loading="lazy" alt="Twitterのリンク" class="image-30"></a>
+        <a href="https://jp.quora.com/profile/Ishii-Takuya" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/1490889680-quora_82507.png" loading="lazy" alt="Quoraのリンク" class="image-30"></a>
+        <a href="/blog" target="_blank" class="instagram w-inline-block"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/blog-icon.png" loading="lazy" alt="ブログのリンク" class="image-30"></a>
       </div>
     </main>
     <!-- フッター -->
